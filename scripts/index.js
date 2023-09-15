60b89fc329e6a925d21fcfeaeca407cf24cbc205
Nicholson Galela <nicholsongalela@gmail,com>
2023-09-15 13:29:28 +0200

    sort books by title

--- scripts/index.js
+++ scripts/index.js
@@ -2,8 +2,8 @@
 
 const books = [
     {"title": "Book 1", "author": "Author A", "year": "2001"},
-    {"title": "Book 2", "author": "Author B", "year": "2002"},
     {"title": "Book 3", "author": "Author C", "year": "2003"},
+    {"title": "Book 2", "author": "Author B", "year": "2002"},
     // Add more books as needed
 ];
 
@@ -16,14 +16,20 @@ let currentBooks = [...books];
 
 // method to display all books from the data file provided
 function displayBooks(books) {
-    console.log("test");
+    console.log("inside");
     libraryBooks.innerHTML = '';
     books.forEach(book => {
-        debugger
+        // debugger
         const li = document.createElement('li');
         li.textContent = `${book.title} by ${book.author} (${book.year})`;
         allLibraryBooks.appendChild(li);
     });
 }
 
+function sortBooksByTitle() {
+    currentBooks.sort((a, b) => a.title.localeCompare(b.title));
+    displayBooks(currentBooks);
+}
+
 displayBooks(currentBooks);
+sortByTitleBtn.addEventListener('click', sortBooksByTitle);
